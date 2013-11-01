abe5de481fee007367b6e15a29d771b7744e0fee
Christopher Bebry <invalidsyntax@gmail.com>
2013-11-01 00:11:29 -0400

    Fixed name typo and added blank functional test.

---
+++ test/functional.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe('Faire Library API', function() {
+	describe('#function()', function() {
+		it('should do stuff.', function(done) {
+			done()
+		})
+	})
+})
