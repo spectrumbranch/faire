066cb73fc2ea43815e5680ac5a63f00417e3459d
Christopher Bebry <invalidsyntax@gmail.com>
2013-11-25 01:51:45 -0500

    When a user clicks the confirmation URL in their email, they now are presented at the Login Page with a message stating their account has been successfully confirmed. This commit closes #1

--- static/js/faire.login.js
+++ static/js/faire.login.js
@@ -9,5 +9,10 @@ var registerSuccessful = function() {
 			.append('Registration was successful! Please check your email for a confirmation link.')
 			.append('<br /><br /><input type="button" value="Okay!" class="button success radius" onclick="$(\'#faireLoginModal\').foundation(\'reveal\', \'close\')"/>');
 		$('#faireLoginModal').foundation('reveal', 'open');
+	} else if (response =='confirmed') {
+		$('#faireLoginModalMessage').empty()
+			.append('Your account has been successfully confirmed! Please log in')
+			.append('<br /><br /><input type="button" value="Okay!" class="button success radius" onclick="$(\'#faireLoginModal\').foundation(\'reveal\', \'close\')"/>');
+		$('#faireLoginModal').foundation('reveal', 'open');
 	}
 };
