63e16cc06bfaa79b414314e92809a07036abb27c
Christopher Bebry <invalidsyntax@gmail.com>
2014-04-06 21:02:13 -0400

    Modified tests to fail more properly.

--- test/functional.js
+++ test/functional.js
@@ -283,9 +283,39 @@ describe('Faire.Tasks API', function() {
 })
 
 describe('Faire.Preferences API', function() {
-	describe('#setTheme()', function() {
-		it('should create the preferences object if it does not exist already', function(done) {
-			assert(false);
+	var user_id_1;
+	var theme_1 = 'default';
+	before(function(done) {
+		var email1 = 'preftests@test.com';
+		var passwrd1 = 'dfdiuw437thudsriweaw3';
+
+		var virt_modules = [];
+		virt_modules.push(Faire.Scurvy);
+
+		var db = require('../lib/models');
+		db.init(virt_modules, function() {
+			console.log('-------- database setup complete --------');
+			Faire.Scurvy.createUser({email: email1, passwrd: passwrd1, status: 'active'}, function(err, userball1) {
+				user_id_1 = userball1.user.id
+				done();
+			});
+		});
+	})
+	describe('#preferences', function() {
+		it('should change the set theme in the preferences object and return the changed object', function(done) {
+			Faire.Preferences.get({ user: user_id_1 }, function(err, preference) {
+				//expect default
+				assert(err == null);
+				
+				assert(preference != null);
+				assert(preference.theme == 'default');
+				
+				Faire.Preferences.setTheme( { user: user_id_1, theme: 'notdefault' }, function(err2, changedPrefs) {
+					assert(err2 == null);
+					assert(changedPrefs != null);
+					assert(changedPrefs.theme == 'notdefault');
+				})
+			});
 		});
 	})
 })
