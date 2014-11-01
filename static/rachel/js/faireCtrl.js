f2b723e559fbe9b15f24731402d5eb8efd7e3441
Rachel Hirshan <acergriseum919@gmail.com>
2014-11-01 17:22:20 -0400

    Add Create New List Section and Sort Section, Hooked up get list data

--- static/rachel/js/faireCtrl.js
+++ static/rachel/js/faireCtrl.js
@@ -1,27 +1,42 @@
 angular.module('faireApp', ['ngTouch'])
 	.controller('faireCtrl', function ($rootScope, $scope, $http) {
 		$scope.lists = [];
-		var listOne = {};
-		listOne.name = 'Groceries';
-		listOne.tasks = [];
-		var taskOne = {};
-		taskOne.name = 'Avocado';
-		var taskTwo = {};
-		taskTwo.name = 'Coffee';
-		listOne.tasks.push(taskOne);
-		listOne.tasks.push(taskTwo);
-		$scope.lists.push(listOne);
+		// var listOne = {};
+		// listOne.name = 'Groceries';
+		// listOne.tasks = [];
+		// var taskOne = {};
+		// taskOne.name = 'Avocado';
+		// var taskTwo = {};
+		// taskTwo.name = 'Coffee';
+		// listOne.tasks.push(taskOne);
+		// listOne.tasks.push(taskTwo);
+		// $scope.lists.push(listOne);
 		
-		var listTwo = {};
-		listTwo.name = 'Movies';
-		listTwo.tasks = [];
-		var movieOne = {};
-		movieOne.name = 'Aladdin';
-		var movieTwo = {};
-		movieTwo.name = 'Beauty & The Beast';
-		listTwo.tasks.push(movieOne);
-		listTwo.tasks.push(movieTwo);
-		$scope.lists.push(listTwo);
+		// var listTwo = {};
+		// listTwo.name = 'Movies';
+		// listTwo.tasks = [];
+		// var movieOne = {};
+		// movieOne.name = 'Aladdin';
+		// var movieTwo = {};
+		// movieTwo.name = 'Beauty & The Beast';
+		// listTwo.tasks.push(movieOne);
+		// listTwo.tasks.push(movieTwo);
+		// $scope.lists.push(listTwo);
+		
+		;(function() {
+			$http({
+				url: '/lists',
+				method: 'GET',
+				data: {}
+			}).success(function(data, status, headers, config) {
+				$scope.lists = data;
+				console.log(data);
+			}).error(function(data, status, headers, config) {
+				console.log('there is an error: ' + status);
+				console.log(data);
+			})
+		})();
+
 	});
 
 
