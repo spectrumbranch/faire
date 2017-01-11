e6b53d705d935b47e25b6f6f4baf1bb2e204aa1d
cbebry <invalidsyntax@gmail.com>
2017-01-11 00:14:57 -0500

    Upgrades hapi to latest version. Marks faire v1.2.0

--- index.js
+++ index.js
@@ -1,40 +1,63 @@
 var Faire = require('./lib');
 
-var Hapi = Faire.Hapi,
-    options = { cors: true };
+var Path = Faire.Path;
+var Hapi = Faire.Hapi;
+var Inert = Faire.Inert;
+var CookieAuth = Faire.CookieAuth;
+var Vision = Faire.Vision;
+
+var options = { cors: true };
+
 var masterConfig = require('./config/config');
 
 var serverConfig = masterConfig.config,
     tlsConfig = masterConfig.tlsconfig,
     mailConfig = masterConfig.mailconfig;
+
+
+Faire.Mailer.init(mailConfig);
         
 if (serverConfig.tls) {
     console.log('Loading tls');
     options.tls = tlsConfig;
 }
 
-var server = new Hapi.Server(serverConfig.hostname, serverConfig.port, options);
-
-Faire.Mailer.init(mailConfig);
-
-server.views({
-    engines: {
-        html: require('handlebars')
-    },
-    path: './lib/views',
-    partialsPath: './lib/views/partials'
+var server = new Hapi.Server({
+  connections: {
+    routes: {
+      files: {
+        relativeTo: Path.join(__dirname, 'static')
+      }
+    }
+  }
 });
 
-server.pack.register(require('hapi-auth-cookie'), function (err) {
-    server.auth.strategy('session', 'cookie', {
-        password: serverConfig.cookie_password,
-        cookie: serverConfig.cookie_name,
-        redirectTo: '/login',
-        isSecure: serverConfig.tls,
-        clearInvalid: true
+server.connection({
+  host: serverConfig.hostname,
+  port: serverConfig.port
+});
+
+
+server.register([Inert, CookieAuth, Vision], (err) => {
+      server.auth.strategy('session', 'cookie', {
+          password: serverConfig.cookie_password,
+          cookie: serverConfig.cookie_name,
+          redirectTo: '/login',
+          isSecure: serverConfig.tls,
+          clearInvalid: true
+      });
+
+      server.route(Faire.Routes.get(Faire));
+
+
+    server.views({
+        engines: {
+            html: require('handlebars')
+        },
+        relativeTo: Path.join(__dirname, '/lib/views'),
+        path: '.',
+        partialsPath: 'partials'
     });
-    //Routes setup
-    server.route(Faire.Routes.get(Faire));
 
     //setup/load modules/plugins here
     var virt_modules = [];
@@ -44,10 +67,15 @@ server.pack.register(require('hapi-auth-cookie'), function (err) {
     db.init(virt_modules, function() {
         console.log('database setup complete');
         
-        //start server
-        server.start();
-        Faire.Auth.setURI(server.info.uri);
-        console.log('Server up at ' + server.info.uri + ' !');
+        server.start(function(err) {
+            if (err) {
+              throw err;
+            }
 
+            Faire.Auth.setURI(server.info.uri);
+            console.log('Server up at ' + server.info.uri + ' !');
+        });
     });
+
+      
 });
